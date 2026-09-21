c2dd3d851c9e67f014f3f66b9133e162bad871f2
rendersomejuice <rendersomejuice@gmail.com>
2026-09-21 16:53:42 +0200

    first commit

---
+++ trheejs1/src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <main>
+        <NavBar></NavBar>
+    </main>
+  )
+}
+
+export default App
---
+++ trheejs1/src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import {NavLinks} from '../constants'
+
+const navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple logo"/>
+            <ul>
+                {NavLinks.map(({label}) => (
+                    <li key={label}>
+                        <a href={label}>{label}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className="flex-center gap-3">
+                <button>
+                    <img src="search.svg" alt="Search"/>
+                </button>
+                <button>
+                    <img src="cart.svg" alt="Cart"/>
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default navbar
