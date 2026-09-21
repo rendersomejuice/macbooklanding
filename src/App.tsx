03ae0ef868f040892b9728e5f0a644813cd2f16a
rendersomejuice <rendersomejuice@gmail.com>
2026-09-21 17:54:41 +0200

    Preparing product viewer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (
     <main>
-        <NavBar></NavBar>
-        <Hero></Hero>
+        <NavBar/>
+        <Hero/>
+        <ProductViewer/>
     </main>
   )
 }
---
+++ src/components/ProductViewer.tsx
@@ -0,0 +1,23 @@
+const ProductViewer = () => {
+  return (
+    <section id="product-viewer">
+        <h2>Take a closer look.</h2>
+        <div className="controls">
+            <p className="info">Macbook Pro 16" in Space Black.</p>
+            <div className="flex-center gap-5 mt-5">
+                <div className="color-control">
+                    <div className="bg-neutral-300"/>
+                    <div className="bg-neutral-900"/>
+                </div>
+                <div className="size-control">
+                    <div><p>14"</p></div>
+                    <div><p>16"</p></div>
+                </div>
+            </div>
+        </div>
+        <p className="text-white text-4xl">Render Canvas</p>
+    </section>
+  )
+}
+
+export default ProductViewer
