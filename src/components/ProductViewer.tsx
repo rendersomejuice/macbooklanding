2cf4ec81f097848e95ca19f9a95b111f6e04b335
rendersomejuice <rendersomejuice@gmail.com>
2026-09-22 10:47:04 +0200

    adding 3d models

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
 import useMacbookStore from '../store'
 import {Canvas} from '@react-three/fiber'
-import { Box } from '@react-three/drei';
+import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14'
 
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacbookStore();
@@ -38,7 +39,9 @@ const ProductViewer = () => {
                 </div>
             </div>
             <Canvas id="canvas" camera={{position : [0,2,5], fov : 50, near : 0.1, far : 100}}>
-                <Box position={[0,0,0]} scale={scale * 10} material-color={'#'+color} />
+                <ambientLight intensity={1}/>
+                <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
+                <OrbitControls enableZoom={false}/>
             </Canvas>
         </section>
     )
