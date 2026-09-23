cef7b17bb680e787415efdacc7dcb5ec14a73696
rendersomejuice <rendersomejuice@gmail.com>
2026-09-23 15:47:38 +0200

    fixed orbitalcamera for mobile

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -40,7 +40,7 @@ const ProductViewer = () => {
                     </div>
                 </div>
             </div>
-            <Canvas id="canvas" camera={{position : [0,2,5], fov : 50, near : 0.1, far : 100}}>
+            <Canvas id="canvas" style={ {'touchAction': 'none'}} camera={{position : [0,2,5], fov : 50, near : 0.1, far : 100}}>
                 <StudioLights/>
                 <ModelSwitcher scale={isMobile ? scale -0.03 : scale} isMobile={isMobile}/>
             </Canvas>
