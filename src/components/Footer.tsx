68f0ef55adcd44a236881bc6f85f0c7968db9f7d
rendersomejuice <rendersomejuice@gmail.com>
2026-09-23 13:43:34 +0200

    final section footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,24 @@
+import {footerLinks} from '../constants'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="info">
+        <p>More ways to shop. Find an Apple store or other retailer near you.</p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright this page is made for learning purposes only</p>
+        <ul>
+          {footerLinks.map(({link, label}) =>(
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
